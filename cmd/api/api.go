77132d257ac6cd923762f2a3ae91c5a86f2ab305
Ruslan Telitsyn <zimpuytel@gmail.com>
2024-08-25 15:14:51 +0300

    products service implementation

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/fozyz/gocom/services/product"
 	"github.com/fozyz/gocom/services/user"
 	"github.com/gorilla/mux"
 )
@@ -25,6 +26,10 @@ func (s *APIServer) Run() error {
 	userStore := user.NewStore(s.db)
 	userHandler := user.NewHandler(userStore)
 	userHandler.RegisterRoutes(subrouter)
+
+	productStore := product.NewStore(s.db)
+	productHandler := product.NewHandler(productStore)
+	productHandler.RegisterRoutes(subrouter)
 
 	log.Println("Listening on", s.addr)
 
--- types/types.go
+++ types/types.go
@@ -8,27 +8,51 @@ type UserStore interface {
 	CreateUser(User) error
 }
 
+type ProductStore interface {
+	GetProducts() ([]Product, error)
+	CreateProduct(Product) error
+}
+
+type Product struct {
+	ID          int       `json:"id"`
+	Name        string       `json:"name"`
+	Description string       `json:"description"`
+	Image       string       `json:"image"`
+	Price       float64       `json:"price"`
+	Quantity    int       `json:"quantity"` 
+	CreatedAt   time.Time `json:"createdAt"`
+}
+
+type ProductPayload struct {
+	Name        string `json:"name" validate:"required"`
+	Description string `json:"description" validate:"required"`
+	Image       string `json:"image"`
+	Price       float64 `json:"price" validate:"required"`
+	Quantity    int `json:"quantity" validate:"required"`
+}
+
 func GetUserByEmail(email string) (*User, error) {
 	return nil, nil
 }
 
 type User struct {
-	ID int `json:"id"`
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-	Email     string `json:"email"`
-	Password  string `json:"password"`
+	ID        int       `json:"id"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"createdAt"`
 }
 
 type RegisterUserPayload struct {
 	FirstName string `json:"firstName" validate:"required"`
 	LastName  string `json:"lastName" validate:"required"`
-	Email     string`json:"email" validate:"required,email"`
+	Email     string `json:"email" validate:"required,email"`
 	Password  string `json:"password" validate:"required,min=3,max=130"`
 }
 
 type LoginUserPayload struct {
-	Email     string`json:"email" validate:"required,email"`
-	Password  string `json:"password" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
 }
+
